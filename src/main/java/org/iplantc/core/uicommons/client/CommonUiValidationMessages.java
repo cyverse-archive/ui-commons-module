bb29d9e6aa204d92999ee83e292a8ee9dbefcd42
Jonathan Strootman <jstroot@iplancollaborative.org>
2013-01-13 12:35:25 -0700

    Moved Validators from de-widgets to ui-commons

--- src/main/java/org/iplantc/core/uicommons/client/CommonUiValidationMessages.java
+++ src/main/java/org/iplantc/core/uicommons/client/CommonUiValidationMessages.java
@@ -5,4 +5,6 @@ import com.google.gwt.i18n.client.Messages;
 public interface CommonUiValidationMessages extends Messages {
 
     String analysisNameValidationMsg();
+
+    String invalidEmail();
 }
