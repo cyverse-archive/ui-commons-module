aa2177a822ed48670915777b676c65ec13dcae48
Jonathan Strootman <jstroot@iplancollaborative.org>
2013-02-05 18:00:17 -0700

    Removed old event and added classes to enable new window mgmt/persistence arch

---
+++ src/main/java/org/iplantc/core/uicommons/client/models/autobeans/WindowType.java
@@ -0,0 +1,17 @@
+package org.iplantc.core.uicommons.client.models.autobeans;
+
+public enum WindowType {
+
+    ABOUT,
+    ANALYSES,
+    APPS,
+    APP_INTEGRATION,
+    APP_WIZARD,
+    DATA,
+    DATA_VIEWER,
+    HELP,
+    IDROP_LITE,
+    NOTIFICATIONS,
+    SIMPLE_DOWNLOAD;
+
+}
