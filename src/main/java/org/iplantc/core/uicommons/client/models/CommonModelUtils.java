45a7e6ad1aacb5955a3ecdea0a99fdec8c8e1724
Jonathan Strootman <jstroot@iplancollaborative.org>
2013-03-08 17:36:01 -0700

    Added utility to create HasId autobeans

---
+++ src/main/java/org/iplantc/core/uicommons/client/models/CommonModelUtils.java
@@ -0,0 +1,18 @@
+package org.iplantc.core.uicommons.client.models;
+
+import com.google.gwt.core.shared.GWT;
+import com.google.web.bindery.autobean.shared.AutoBean;
+import com.google.web.bindery.autobean.shared.AutoBeanCodex;
+import com.google.web.bindery.autobean.shared.Splittable;
+import com.google.web.bindery.autobean.shared.impl.StringQuoter;
+
+public class CommonModelUtils {
+    private static final CommonModelAutoBeanFactory factory = GWT.create(CommonModelAutoBeanFactory.class);
+
+    public static HasId createHasIdFromString(String id) {
+        Splittable hasIdSplittable = StringQuoter.createSplittable();
+        StringQuoter.create(id).assign(hasIdSplittable, "id");
+        AutoBean<HasId> hasId = AutoBeanCodex.decode(factory, HasId.class, hasIdSplittable);
+        return hasId.as();
+    }
+}
