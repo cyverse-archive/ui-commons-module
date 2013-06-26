771bab650c6bc4bba75807104a11925e7d3687c6
Tony Edgin <tedgin@iplantcollaborative.org>
2013-06-26 09:15:49 -0700

    Added Map-like get() method to DiskResourceExistMap

--- src/main/java/org/iplantc/core/uicommons/client/models/diskresources/DiskResourceAutoBeanFactory.java
+++ src/main/java/org/iplantc/core/uicommons/client/models/diskresources/DiskResourceAutoBeanFactory.java
@@ -6,7 +6,9 @@ import org.iplantc.core.uicommons.client.models.diskresources.RestoreResponse.Re
 import com.google.gwt.core.shared.GWT;
 import com.google.web.bindery.autobean.shared.AutoBean;
 import com.google.web.bindery.autobean.shared.AutoBeanFactory;
+import com.google.web.bindery.autobean.shared.AutoBeanFactory.Category;
 
+@Category(DiskResourceExistMap.Category.class)
 public interface DiskResourceAutoBeanFactory extends AutoBeanFactory {
 
     static final DiskResourceAutoBeanFactory INSTANCE = GWT.create(DiskResourceAutoBeanFactory.class);
