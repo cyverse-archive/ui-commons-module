e9542e8fc487a231349f9abab2b2ece0e1460c83
Sriram Srinivasan <sriram@iplantcollaborative.org>
2013-07-03 12:40:29 -0700

    refactor code. Add file / folder enum types

---
+++ src/main/java/org/iplantc/core/uicommons/client/models/diskresources/TYPE.java
@@ -0,0 +1,12 @@
+package org.iplantc.core.uicommons.client.models.diskresources;
+
+/**
+ * Enum type definition for file and folder
+ * 
+ *  * @author sriram
+ *
+ */
+
+public enum TYPE {
+    FILE, FOLDER;
+}
