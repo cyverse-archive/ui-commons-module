3df7b6bb7633a2635137ea94a14efdbef9bc4c93
Tony Edgin <tedgin@iplantcollaborative.org>
2013-07-05 15:29:46 -0700

    tool request model and service changes
    The tool installation request endpoint was created, along with the data
    model auto beans required to support it.

--- src/main/java/org/iplantc/core/uicommons/client/gin/ServicesInjector.java
+++ src/main/java/org/iplantc/core/uicommons/client/gin/ServicesInjector.java
@@ -1,6 +1,7 @@
 package org.iplantc.core.uicommons.client.gin;
 
 import org.iplantc.core.uicommons.client.services.DiskResourceServiceFacade;
+import org.iplantc.core.uicommons.client.services.ToolRequestProvider;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.inject.client.GinModules;
@@ -12,5 +13,7 @@ public interface ServicesInjector extends Ginjector {
     final ServicesInjector INSTANCE = GWT.create(ServicesInjector.class);
 
     DiskResourceServiceFacade getDiskResourceServiceFacade();
+
+    ToolRequestProvider getToolRequestServiceProvider();
 
 }
--- src/main/java/org/iplantc/core/uicommons/client/gin/ServicesModule.java
+++ src/main/java/org/iplantc/core/uicommons/client/gin/ServicesModule.java
@@ -1,6 +1,7 @@
 package org.iplantc.core.uicommons.client.gin;
 
 import org.iplantc.core.uicommons.client.services.DiskResourceServiceFacade;
+import org.iplantc.core.uicommons.client.services.ToolRequestProvider;
 
 import com.google.gwt.inject.client.AbstractGinModule;
 import com.google.inject.Singleton;
@@ -10,6 +11,7 @@ final class ServicesModule extends AbstractGinModule {
     @Override
     protected void configure() {
         bind(DiskResourceServiceFacade.class).in(Singleton.class);
+        bind(ToolRequestProvider.class).in(Singleton.class);
     }
 
 }
