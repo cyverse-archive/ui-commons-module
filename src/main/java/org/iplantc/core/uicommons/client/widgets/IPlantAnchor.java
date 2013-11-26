63e3158fb4098c489e4db90892250f88b88be025
Jonathan Strootman <jstroot@iplantcollaborative.org>
2013-11-26 15:44:41 -0700

    Refactor and cleanup of IPlantAnchor class.

--- src/main/java/org/iplantc/core/uicommons/client/widgets/IPlantAnchor.java
+++ src/main/java/org/iplantc/core/uicommons/client/widgets/IPlantAnchor.java
@@ -7,29 +7,29 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.HasClickHandlers;
-import com.google.gwt.event.dom.client.HasMouseOutHandlers;
-import com.google.gwt.event.dom.client.HasMouseOverHandlers;
-import com.google.gwt.event.dom.client.MouseOutEvent;
-import com.google.gwt.event.dom.client.MouseOutHandler;
-import com.google.gwt.event.dom.client.MouseOverEvent;
-import com.google.gwt.event.dom.client.MouseOverHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
-import com.google.gwt.user.client.Event;
+
 import com.sencha.gxt.core.client.dom.XDOM;
 import com.sencha.gxt.widget.core.client.Component;
 
 /**
  * A widget to create html link like <a> tag
  * 
- * TODO JDS This class needs to transformed into a cell-backed widget.
  * @author sriram
  * 
  */
-public class IPlantAnchor extends Component implements HasClickHandlers, HasMouseOverHandlers,
-        HasMouseOutHandlers {
+public class IPlantAnchor extends Component implements HasClickHandlers {
 
     private final IPlantAnchorAppearance appearance;
+
+    public IPlantAnchor(String text, int width, IPlantAnchorAppearance appearance, ClickHandler handler) {
+        this(text, width, appearance);
+
+        if (handler != null) {
+            addClickHandler(handler);
+        }
+    }
 
     /**
      * 
@@ -37,42 +37,32 @@ public class IPlantAnchor extends Component implements HasClickHandlers, HasMous
      * 
      * @param text text to display
      */
-    public IPlantAnchor(String text, int width, IPlantAnchorAppearance appearance, ClickHandler handler) {
+    public IPlantAnchor(String text, int width, IPlantAnchorAppearance appearance) {
         this.appearance = appearance;
         SafeHtmlBuilder sb = new SafeHtmlBuilder();
         this.appearance.render(sb);
         setWidth(width);
         setElement(XDOM.create(sb.toSafeHtml()));
         setText(text);
-        sinkEvents(Event.ONCLICK);
-        sinkEvents(Event.ONMOUSEOVER);
-        sinkEvents(Event.ONMOUSEOUT);
-        addEventHandler(handler);
-    }
-
-    public IPlantAnchor(String text, int width, ClickHandler handler) {
-        this(text, width, (IPlantAnchorAppearance)GWT.create(IPlantAnchorAppearance.class), handler);
-    }
-
-
-    private void addEventHandler(ClickHandler handler) {
-        addClickHandler(handler);
-        addMouseOutHandler(new MouseOutHandler() {
+        /*addDomHandler(new MouseOutHandler() {
 
             @Override
             public void onMouseOut(MouseOutEvent event) {
                 IPlantAnchor.this.appearance.onMouseOut(getElement());
             }
-        });
-        addMouseOverHandler(new MouseOverHandler() {
+        }, MouseOutEvent.getType());
+        addDomHandler(new MouseOverHandler() {
             
             @Override
             public void onMouseOver(MouseOverEvent event) {
                 IPlantAnchor.this.appearance.onMouseOver(getElement());
             }
-        });
+        }, MouseOverEvent.getType());*/
     }
 
+    public IPlantAnchor(String text, int width, ClickHandler handler) {
+        this(text, width, (IPlantAnchorAppearance)GWT.create(IPlantAnchorAppearance.class), handler);
+    }
 
     public void setText(String text) {
         appearance.onUpdateText(getElement(), text);
@@ -81,17 +71,6 @@ public class IPlantAnchor extends Component implements HasClickHandlers, HasMous
     @Override
     public HandlerRegistration addClickHandler(ClickHandler handler) {
         return addDomHandler(handler, ClickEvent.getType());
-    }
-
-
-    @Override
-    public HandlerRegistration addMouseOutHandler(MouseOutHandler handler) {
-        return addDomHandler(handler, MouseOutEvent.getType());
-    }
-
-    @Override
-    public HandlerRegistration addMouseOverHandler(MouseOverHandler handler) {
-        return addDomHandler(handler, MouseOverEvent.getType());
     }
 
 }
