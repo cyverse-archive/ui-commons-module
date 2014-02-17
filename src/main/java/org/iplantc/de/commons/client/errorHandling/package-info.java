b699792ab656db27848a60f3784ef0664a4d62c8
Jonathan Strootman <jstroot@iplantcollaborative.org>
2014-02-17 13:44:42 -0700

    Consolidating data model

--- src/main/java/org/iplantc/de/commons/client/errorHandling/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * A package which contains all UI error handling
- * 
- * TODO JDS Move the {@link org.iplantc.de.commons.client.ErrorHandler} class to this package, and rename package to "error"
- * @author jstroot
- * 
- */
-package org.iplantc.de.commons.client.errorHandling;
